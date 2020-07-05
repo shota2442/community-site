45cf27effb9e6cdba30b4f1c945a33fe988e704f
shota.otuka <v.g.d.s-ml@i.softbank.jp>
2020-07-05 10:31:18 +0000

    microposts.migrate

--- db/migrate/20200624155503_create_microposts.rb
+++ db/migrate/20200624155503_create_microposts.rb
@@ -2,10 +2,11 @@ class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
       t.string :content
-      t.references :user, type: :bigint, foreign_key: true
-      t.references :community, type: :bigint, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :community, foreign_key: true
 
       t.timestamps
+      
     end
   end
 end
